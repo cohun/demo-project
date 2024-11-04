679f015a955ab82b84fff31cda0c5bccff75728f
Colin Hunwald <colin.hunwald@gmail.com>
2024-11-04 15:04:07 +0100

    Add more functions to dataset

--- src/dataset.py
+++ src/dataset.py
@@ -2,7 +2,22 @@ def multiply(a, b):
     return a * b
 
 
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def divide(a, b):
+    return a / b
+
+
 a = 10
 b = 5
 
 print(multiply(a, b))
+print(add(a, b))
+print(subtract(a, b))
+print(divide(a, b))
